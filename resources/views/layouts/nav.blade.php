8fe3fae3897ce60caf82b09eceff20d49df7e564
codeachun <codeachun@gmail.com>
2019-08-14 09:55:54 +0800

    安裝 laravel-admin 後台

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #00AA88;">
+<nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #008866;">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
