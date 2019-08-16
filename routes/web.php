0abeea8fedb3bb561e8159af797b11c66cec97e8
codeachun <codeachun@gmail.com>
2019-08-16 13:51:32 +0800

    微調購物車的傳送方式 刪除某個購物品項

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
  */
 Route::pattern('product', '[0-9]+');
+Route::pattern('id', '[0-9]+');
 
 Route::get('/', 'ProductController@index')->name('index');
 Route::get('/product', 'ProductController@index')->name('product.index');
@@ -18,6 +19,7 @@ Route::get('/product/{product}', 'ProductController@show')->name('product.show')
 
 Route::post('/cart/store', 'CartController@store')->name('cart.store');
 Route::get('/cart', 'CartController@index')->name('cart.index');
+Route::delete('/cart/{id}', 'CartController@destroy')->name('cart.destroy');
 
 Auth::routes();
 
